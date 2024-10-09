330fb03125336140ea3f9d68867bbfadcb1503c1
eduardscutariu <scutariueduard22@stud.ase.ro>
2024-10-09 21:19:53 +0300

    seminar2

---
+++ Seminar2/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Seminar2"
+include(":app")
