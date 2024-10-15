4ee57ab7752c152189e7825fec377c0d72b079fb
eduardscutariu <scutariueduard22@stud.ase.ro>
2024-10-15 17:57:03 +0300

    seminar3

---
+++ Seminar3_1099/app/src/main/java/com/example/seminar3_1099/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.seminar3_1099;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+
+    public void SecondActivity(View view) {
+        Intent it = new Intent(this,SecondActivity.class);
+        it.putExtra("text","text trimis din prima activitate");
+        it.putExtra("numar1",123);
+        it.putExtra("numar2",123);
+        startActivity(it);
+    }
+}
